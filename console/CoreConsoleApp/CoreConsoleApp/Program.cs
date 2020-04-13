8716dc2b9de5d515aa245669c821da38ffb2181d
aodegov <a.odegov@svitla.com>
2020-04-13 18:17:03 +0300

    Repository Initialization

---
+++ console/CoreConsoleApp/CoreConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Div(123));
+        }
+
+        static int Div(int num)
+        {
+            return num != 0 ? (num % 10 + Div(num / 10)) : 0;
+        }
+    }
+}

b5c85dfe763696d6b4afe630a131d36922b8fd4a
Anton Odegov <altecsyst@gmail.com>
2020-05-25 22:29:33 +0300

    Added async-await project

---
+++ async-await-test/ConsoleTest/AsyncAwaitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    public class AsyncAwaitTest
+    {
+        public AsyncAwaitTest()
+        {
+            // GetDelayAsync();
+        }
+
+        public async Task GetDelayAsync()
+        {
+            Console.WriteLine($"Async Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(1000);
+            Console.WriteLine($"Async AD Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+        } 
+    }
+}
---
+++ async-await-test/ConsoleTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var th = Thread.CurrentThread;
+            Console.WriteLine($"Main Thread Id: {Thread.CurrentThread.ManagedThreadId}");
+            AsyncAwaitTest asyncAwaitTest = new AsyncAwaitTest();
+            await asyncAwaitTest.GetDelayAsync();
+            Console.WriteLine($"Main Thread Id AD: {Thread.CurrentThread.ManagedThreadId}");
+            Console.Read();
+
+            //ConcurrentCollection cc = new ConcurrentCollection();
+            //cc.RunConcurrency();
+
+            //A a = new A();
+            //a.ShowMessage();
+
+            //B b = new B();
+            //b.ShowMessage();
+
+            //A a2 = new B();
+            //a2.ShowMessage();
+
+            //A a3 = (A)new B();
+            //a3.ShowMessage();
+        }
+    }
+
+    class A
+    {
+        public virtual void ShowMessage()
+        {
+            Console.WriteLine("Class A");
+        } 
+    }
+
+    class B : A
+    {
+        public override void ShowMessage()
+        {
+            Console.WriteLine("Class B");
+        }
+        
+    }
+} 

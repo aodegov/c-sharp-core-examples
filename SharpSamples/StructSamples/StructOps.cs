d00756d52cee61f633236fa0460fa5922b2f7dcc
aodegov <a.odegov@svitla.com>
2020-06-29 14:42:52 +0300

    Solution Reorganization

---
+++ SharpSamples/StructSamples/StructOps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StructSamples
+{
+    public interface IStructOps
+    {
+        void ShowCoordinates();
+    }
+
+    public class StructOps
+    {
+        struct Coordinate
+        {
+            public int X;
+            public int Y;
+        }
+
+        struct ThirdDimension: IStructOps /*StructOps : struct can't inherit struct*/
+        {
+            public int Z;
+
+            public void ShowCoordinates()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+
+}

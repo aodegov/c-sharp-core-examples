dc52ee8f66588571bfd97be1997322dd6576300c
aodegov <a.odegov@svitla.com>
2020-06-29 15:22:55 +0300

    Updated struct operations with comments

--- SharpSamples/StructSamples/StructOps.cs
+++ SharpSamples/StructSamples/StructOps.cs
@@ -4,25 +4,59 @@ namespace StructSamples
 {
     public interface IStructOps
     {
-        void ShowCoordinates();
+        void ShowCoordinate();
     }
 
     public class StructOps
     {
         struct Coordinate
         {
-            public int X;
-            public int Y;
+            // Struct constructor
+            public Coordinate(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public int X { get; }
+            public int Y { get; }
+
+            public void SumCoordinates()
+            {
+                Console.WriteLine($"{this.X + this.Y}");
+            }
         }
 
         struct ThirdDimension: IStructOps /*StructOps : struct can't inherit struct*/
         {
             public int Z;
-
-            public void ShowCoordinates()
+             
+            public void ShowCoordinate()
             {
-                throw new NotImplementedException();
+                Console.WriteLine($"{Z}");
             }
+          
+            Coordinate c;
+            /*
+             * You can't set value from one structure,
+             * when inside anoter structure
+             * c.X = 5; - exception
+             */
+        }
+
+        void StructProcessing()
+        {
+            /* Before call any fields or struct methods.
+             * you have to initialize struct variable
+             * with constructor like this:  
+             * Coordinate p = new Coordinate(10, 20);
+             * Or with just delaration like this:
+             * ThirdDimension td;
+             */
+            Coordinate p = new Coordinate(10, 20);
+            p.SumCoordinates();
+            ThirdDimension td;
+            td.Z = 20;
         }
     }
 

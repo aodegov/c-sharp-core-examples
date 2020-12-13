9a2bae9e1d0d91fe0663a929407098deadbf0e6c
anton <altecsyst@gmail.com>
2020-12-13 22:32:14 +0200

    updated with dictionary ops

---
+++ SharpSamples/Processor/SampleService.cs
@@ -0,0 +1,28 @@
+﻿using DictionarySamples;
+using System;
+
+namespace SamplesProcessor
+{
+    public class SampleService
+    {
+        public void DoWork()
+        {
+            try
+            {
+                // StringOps so = new StringOps();
+                // var str = so.StringFormatOps();
+
+                var key = Guid.NewGuid().ToString();
+                DictionaryOps doOps = new DictionaryOps();
+                doOps.DoAdd(key, Guid.NewGuid().ToString());
+                doOps.DoRemove(key, Guid.NewGuid().ToString());
+                doOps.DoAdd(key, Guid.NewGuid().ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}

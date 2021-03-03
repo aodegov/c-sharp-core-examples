1248ad40ec4f4185ae4a64fc5926c415feac259f
anton <altecsyst@gmail.com>
2021-03-03 21:35:34 +0200

    updated with date / time ops

---
+++ SharpSamples/DateTimeOps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateTimeOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
